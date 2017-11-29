57158a63ae144985a992f7322cf06ac02b9c1701
Aparna-Rajesh <30874258+Aparna-Rajesh@users.noreply.github.com>
2017-11-29 18:58:38 -0500

    Add files via upload

---
+++ BrunsErupAutonParkv1.java
@@ -0,0 +1,129 @@
+package org.firstinspires.ftc.teamcode.BrunswickErup;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by aparn on 11/8/2017.
+ */
+@Autonomous(name = "Autonomous: BrunsPark", group = "Autonomous")
+public class BrunsErupAutonParkv1 extends LinearOpMode{
+    BrunsErupHardware BabyElle = new BrunsErupHardware();
+    private ElapsedTime runtime = new ElapsedTime();
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1120 ;    // eg: TETRIX Motor Encoder TODO: is this right?
+    static final double     DRIVE_GEAR_REDUCTION    = 4.0 ;     // This is < 1.0 if geared UP TODO: MAKE SURE THIS IS RIGHT
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.8;  //TODO: make faster if team finds its moving too slow
+    static final double     TURN_SPEED              = 0.6;
+
+    enum States {
+        //ResetBot,
+        RunToTriangle
+    }
+
+    @Override
+    public void runOpMode(){
+
+        BabyElle.init(hardwareMap);
+
+        telemetry.addData("", "Resetting Motors");
+        telemetry.update();
+
+        BabyElle.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BabyElle.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BabyElle.flipper.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        telemetry.addData("", "Reset complete");
+        telemetry.update();
+
+        waitForStart();
+
+        runStates(States.RunToTriangle); //TODO: is that even right
+
+    }
+
+    public void runStates(States currentState){
+        switch(currentState){
+
+            case RunToTriangle:
+                telemetry.addData("", "Running to Triangle");
+                telemetry.update();
+
+                encoderDrive(0.6, 3, 3, 5);
+
+                telemetry.addData("", "Running to triangle complete");
+                telemetry.update();
+                break;
+
+            default:
+                telemetry.addData("", "Default State");
+                telemetry.update();
+
+                BabyElle.leftDrive.setPower(0);
+                BabyElle.rightDrive.setPower(0);
+                BabyElle.flipper.setPower(0);
+                break;
+        }
+    }
+
+
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = BabyElle.leftDrive.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = BabyElle.rightDrive.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            BabyElle.leftDrive.setTargetPosition(newLeftTarget);
+            BabyElle.rightDrive.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            BabyElle.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            BabyElle.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            BabyElle.leftDrive.setPower(Math.abs(speed));
+            BabyElle.rightDrive.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (BabyElle.leftDrive.isBusy() && BabyElle.rightDrive.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+                        BabyElle.leftDrive.getCurrentPosition(),
+                        BabyElle.rightDrive.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            BabyElle.leftDrive.setPower(0);
+            BabyElle.rightDrive.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            BabyElle.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            BabyElle.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after each move
+        }
+    }
+
+}
---
+++ Nov15VuforiaTest.java
@@ -0,0 +1,111 @@
+package org.firstinspires.ftc.teamcode.VuforiaTesting;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+/**
+ * Created by aparn on 11/15/2017.
+ */
+
+@Autonomous(name="Vuforia Test 1", group ="Concept")
+public class Nov15VuforiaTest extends LinearOpMode{
+
+
+        public static final String TAG = "Vuforia VuMark Sample";
+
+        OpenGLMatrix lastLocation = null;
+
+        /**
+         * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+         * localization engine.
+         */
+        VuforiaLocalizer vuforia;
+
+        @Override public void runOpMode() {
+
+            int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+            VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+
+            parameters.vuforiaLicenseKey = "AUQOzBX/////AAAAGS1wJd7EREe1kg+D56U0glhCT0KQPvq3iDmmV+QkzyyXJu" +   //THIS IS MY OWN VUFORIA KEY
+                    "xYJUfImxbgFKx+8k04CWZ+wx6JimAkMwTgCPPf0SIr8vxC7VdSU4Hml965bB3XYS6X4v+F8Jo2/QpyZlO+QsM" +
+                    "o073blzaBiZu6HeoiPRmUJQLT7YoRo1Sh7PLsmgSs1FQDavg/mNvUiCav+lGp/hxhrn3O7R1i0lKTHRsX7xh8" +
+                    "DKm1qqCCR4yakr6d52+j9rI5FATOjoGgaQBeHK7zMk7yoPi7XtFsTQVWb6pAuP3HLqzi+rejEL5HTfZgnzgCw" +
+                    "02OhIffzcBZTk9CYEGeHZFNr9VeFA1evXhRtDo/f6JgC1GrDcWW1FfTPUeSehUB\n";
+
+
+            parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+            this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+            /**
+             * Load the data set containing the VuMarks for Relic Recovery. There's only one trackable
+             * in this data set: all three of the VuMarks in the game were created from this one template,
+             * but differ in their instance id information.
+             * @see VuMarkInstanceId
+             */
+            VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+            VuforiaTrackable relicTemplate = relicTrackables.get(0);
+            relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+
+            telemetry.addData(">", "Coach, Press play to begin.");
+            telemetry.update();
+            waitForStart();
+
+            relicTrackables.activate();
+
+            while (opModeIsActive()) {
+
+                RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+
+                if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
+                    telemetry.addData("VuMark", "%s visible", vuMark);
+
+                    OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
+                    telemetry.addData("Pose", format(pose));
+
+
+                    if (pose != null) {
+                        VectorF trans = pose.getTranslation();
+                        Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+                        // Extract the X, Y, and Z components of the offset of the target relative to the robot
+                        double tX = trans.get(0);
+                        double tY = trans.get(1);
+                        double tZ = trans.get(2);
+
+                        // Extract the rotational components of the target relative to the robot
+                        double rX = rot.firstAngle;
+                        double rY = rot.secondAngle;
+                        double rZ = rot.thirdAngle;
+                    }
+                }
+                else {
+                    telemetry.addData("VuMark", "not visible.");
+                }
+
+                telemetry.update();
+
+
+            }
+        }
+
+        String format(OpenGLMatrix transformationMatrix) {
+            return (transformationMatrix != null) ? transformationMatrix.formatAsTransform() : "null";
+        }
+
+
+}
